43787f42cf9995896137aca75456ea66649143cc
Tux3er <isma.tux3er@gmail.com>
2024-02-11 22:13:47 +0100

    🎨 Improving open components script

--- .netlify/functions-internal/ssr/chunks/privacy_gTUj3V-N.mjs
+++ .netlify/functions-internal/ssr/chunks/privacy_gTUj3V-N.mjs
@@ -1,5 +1,5 @@
 export { renderers } from '../renderers.mjs';
 
-const page = () => import('./pages/privacy_cKr3D1Kd.mjs');
+const page = () => import('./pages/privacy_29bKt6nP.mjs');
 
 export { page };
--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,12 +1,12 @@
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_-GcFEJEu.mjs';
+import { manifest } from './manifest_Q8BYV0OV.mjs';
 import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./chunks/generic_uieeLu4s.mjs');
 const _page1 = () => import('./chunks/sendEmail_N-n9sv5y.mjs');
 const _page2 = () => import('./chunks/index_UXwPRLvr.mjs');
-const _page3 = () => import('./chunks/privacy_yS0vdxUY.mjs');
+const _page3 = () => import('./chunks/privacy_gTUj3V-N.mjs');
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/api/sendEmail.json.ts", _page1],
@@ -20,7 +20,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "e3c829a1-149d-4e66-bcec-dacf0f992865"
+    "middlewareSecret": "47f39734-1c8d-479c-b638-724920856301"
 };
 const _exports = serverEntrypointModule.createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
