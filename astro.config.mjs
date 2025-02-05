6733e15edbd9287a15dac3b635e42044d834b39b
Tux3er <isma.tux3er@gmail.com>
2025-02-05 21:40:26 +0100

    ➕Migrating to vercel again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import netlify from "@astrojs/netlify/functions";
-import vercelServerless from '@astrojs/vercel/serverless';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel()
+  adapter: netlify()
 });
